6dd15b2d115389c9e8322f150dc2496026221301
Sorbopriyo Roy <sorbopriyo@gmail.com>
2024-01-17 09:24:47 +0530

    Bug fixes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,25 @@
 import { Server, Socket } from "socket.io";
 import http from 'http';
-const express = require('express');
+import express from 'express';
+import { UserManager } from "./managers/UserManager";
 
 const app = express();
 const server = http.createServer(http);
 
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
+
+const userManager = new UserManager();
 
 io.on('connection', (socket: Socket) => {
   console.log('a user connected');
+  userManager.addUser("randomName", socket);
+  socket.on("disconnect", () => {
+    userManager.removeUser(socket.id)
+  })
 });
 
 server.listen(3000, () => {
